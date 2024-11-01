350c851cca561f07fb89b66a825a2e18afa5635e
Artur Coelho <artur.coelho@tasktop.com>
2024-11-01 13:20:20 -0700

    fix: [OPS-13143] add ignore changes aws_emr_cluster.core_instance_fleet

--- main.tf
+++ main.tf
@@ -321,6 +321,7 @@ resource "aws_emr_cluster" "this" {
     ignore_changes = [
       kerberos_attributes, # Since the API does not return the actual values for Kerberos configurations
       step,                # Ignore outside changes to running cluster steps
+      core_instance_fleet[0].target_on_demand_capacity, # Ignore changes to target_on_demand_capacity inside core_instance_fleet
     ]
   }
 }
